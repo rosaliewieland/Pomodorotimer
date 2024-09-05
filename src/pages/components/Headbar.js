874cc69a5bf819358fa1f506fb83d8fae644617f
rosiex <Rosalie.Wieland@student.reutlingen-university.de>
2024-09-05 16:59:52 +0200

    created basic outline for navigation bar and basic outline for interaction window in the middle of the website

--- src/pages/components/Headbar.js
+++ src/pages/components/Headbar.js
@@ -1,2 +1,29 @@
+// components/Headbar.js
 import React from 'react';
-import styles from '../styles/Headbar.css';
+import styles from '../../styles/Headbar.module.css'; // Import CSS Module
+
+const Headbar = () => {
+  return (
+    <header className={styles.header}>
+ 
+            <nav className={styles.nav}>
+
+                <div className={styles['logo-container']}>
+                    <button className={styles['logo-button']} onClick={() => handleScroll("home")}> <img src='../../../Pomodoro.svg' className={styles['logo']} alt="Logo" /></button>
+                    
+               
+                </div>
+
+                <ul className={styles.menu}>
+                <li className={styles['menu-item']}><a href="../">Settings</a></li>
+                <li className={styles['menu-item']}><a href="#">login</a></li>
+                <li className={styles['menu-item']}><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+         
+    </header>
+    
+  );
+};
+
+export default Headbar;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
-
-
 import { Inter } from "next/font/google";
+import HeaderBar from "./components/Headbar";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -15,6 +14,17 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/pomodoro.svg" />
       </Head>
+
+      <HeaderBar></HeaderBar>
+
+      <body>
+        <div class="center-box">
+          <h1>Welcome!</h1>
+          <p>This is a responsive light pink box in the center of the page.</p>
+        </div>
+      </body>
+
+     
     </>
   )
 }
